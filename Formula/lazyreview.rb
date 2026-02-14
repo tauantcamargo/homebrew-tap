00f90e9683ebe41cbe49755bac1be03678ddf38e
Tauan Camargo <lukyhenson@gmail.com>
2026-02-13 21:00:17 -0300

    chore: update lazyreview to 1.0.57

--- Formula/lazyreview.rb
+++ Formula/lazyreview.rb
@@ -1,8 +1,8 @@
 class Lazyreview < Formula
   desc "TUI code review tool for GitHub PRs"
   homepage "https://github.com/tauantcamargo/lazyreview"
-  url "https://registry.npmjs.org/lazyreview/-/lazyreview-1.0.56.tgz"
-  sha256 "795fc28969045f08a0b3c5bb5184680b163bb801262d3b679debfe514504a95e"
+  url "https://registry.npmjs.org/lazyreview/-/lazyreview-1.0.57.tgz"
+  sha256 "40aaab1ce9761d5ec295c7a6f14fbda6fa192f74d9788e4c69cabeb5b5845d7a"
   license "MIT"
 
   depends_on "node"
